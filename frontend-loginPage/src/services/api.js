537ed1f3b27fa523534b9bae35f4515c7ba16509
bhanuprasad-2503 <bellalabhanuprasad@gmail.com>
2025-11-07 19:58:52 +0530

    Add files via upload

---
+++ frontend-loginPage/src/services/api.js
@@ -0,0 +1,4 @@
+import axios from 'axios'
+const api = axios.create({ baseURL: import.meta.env.VITE_API_BASE || 'http://localhost:8080' })
+api.interceptors.request.use(cfg=>{ const t=localStorage.getItem('nf_token'); if(t) cfg.headers.Authorization='Bearer '+t; return cfg })
+export default api
