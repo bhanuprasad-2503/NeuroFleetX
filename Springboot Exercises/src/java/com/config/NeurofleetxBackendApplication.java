ce3d28f147540404c8962587a0a54ec880effadb
bhanuprasad-2503 <bellalabhanuprasad@gmail.com>
2025-11-10 16:36:30 +0530

    Add files via upload

---
+++ Springboot Exercises/src/java/com/config/NeurofleetxBackendApplication.java
@@ -0,0 +1,11 @@
+package com.neurofleetx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurofleetxBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NeurofleetxBackendApplication.class, args);
+    }
+}
